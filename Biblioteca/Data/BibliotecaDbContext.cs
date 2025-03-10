b6d63acb6aef0879dbb63a9eef5b89e96e2d65ed
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-03-10 16:45:29 +0100

    Aggiungere i file di progetto.

---
+++ Biblioteca/Data/BibliotecaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Biblioteca.Models;
+
+namespace Biblioteca.Data
+{
+    public class BibliotecaDbContext : DbContext
+    {
+        public BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options) : base(options) { }
+
+        public DbSet<Libro> Libri { get; set; } = null!;
+        }
+       
+}
+        
+
+    
+
+
